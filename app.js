017e12afac9dc514691e218c2f23a8b86ab5e87a
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2026-06-11 11:05:18 +0530

    improved the number of api calls made per 30sec

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ import {authRouter} from './src/routes/Auth/auth.routes.js';
 import { requestLimiter } from "./src/middlewares/requestLimiter.middleware.js";
 import { authProtectedRouter } from './src/routes/Auth/protectedAuth.routes.js';
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
+import cors from 'cors'
+
 
 const app= express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 app.use("/api", requestLimiter);
 app.use("/api", authRouter);
 app.use("/api", authProtectedRouter);
--- src/middlewares/requestLimiter.middleware.js
+++ src/middlewares/requestLimiter.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const requestLimiter = rateLimit({
   windowMs: 1 * 30 * 1000,
-  max: 5,
+  max: 20,
   message: {
     success: false,
     message: "Too many requests. Take a breath and try again.",
